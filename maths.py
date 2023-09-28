b09c590d359374aee3b63d3a118e07ecc4bb8e3a
Antony <antony.a.salta@gmail.com>
2023-09-28 14:57:15 +0100

    added maths.py

---
+++ maths.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a second number: "))
+print(str(num1) +" + " + str(num2) + " = " + str(num1 + num2))
+print(str(num1) +" * " + str(num2) + " = " + str(num1 * num2))
+print(str(num1) +" / " + str(num2) + " = " + str(num1 / num2))
+print(str(num1) +" % " + str(num2) + " = " + str(num1 % num2))
+print(str(num1) +" ^ " + str(num2) + " = " + str(num1 ** num2))
