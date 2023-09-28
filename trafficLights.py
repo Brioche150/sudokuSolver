01658d03d966ccd083af012a67baae368600b2ff
Antony <antony.a.salta@gmail.com>
2023-09-28 14:30:31 +0100

    finished testing with int and string conversion with concatenation

--- trafficLights.py
+++ trafficLights.py
@@ -1,8 +1,9 @@
 def printState():
-    print("Red Light is " + str(redLight))
-    print("Yellow Light is "+ str(yellowLight))
-    print("Green Light is "+ str(greenLight))
-    print(type(redLight))
+	print("Red Light is " + str(redLight))
+	print("Yellow Light is "+ str(yellowLight))
+	print("Green Light is "+ str(greenLight))
+	print(type(redLight))
+	print("3"+str(5))
 
 redLight = 1
 yellowLight = 0
