907cd43445d5e750afa7a19a87ed9052f3cf805a
r23391as <antony.salta@student.manchester.ac.uk>
2023-11-07 12:08:07 +0000

    Added command to the buttons

--- oxo.py
+++ oxo.py
@@ -4,12 +4,14 @@ def configure_window():
     window.title("O X O")
     window.geometry("300x300")
 
+def handle_button_click(button_number):
+    print(button_number)
+
 def create_buttons():
-   global squares
    for i in range(len(squares)):
-       square = Button(window, image =available)
-       square.place(x=100 * (i%3),y=100 * int(i/3))
-       square.config(width = 100, height = 100)
+       squares[i] = Button(window, image =available, command=lambda n=i:handle_button_click(n))
+       squares[i].place(x=100 * (i%3),y=100 * int(i/3))
+       squares[i].config(width = 100, height = 100)
 
 window = Tk()
 configure_window()
@@ -19,5 +21,5 @@ ButtonP2 = PhotoImage(file = "myButtonP2.png")
 winner = PhotoImage(file = "winner.png")
 squares = [None for i in range(9)]
 create_buttons()
-
+print(squares)
 window.mainloop()
